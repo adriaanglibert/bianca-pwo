435d652513388290b87ac08ed6418e8a995072ef
adrigl@arteveldehs.be <adrigl@arteveldehs.be>
2021-12-14 16:23:33 +0100

    ♻ Use defaultValue instead of ref TimeRange

--- src/components/TimeRange.jsx
+++ src/components/TimeRange.jsx
@@ -1,21 +1,15 @@
-import React, {useRef, useEffect} from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Label from './Label';
 import styles from './TimeRange.module.scss';
 
+const formatTime = (time) => {
+    return `${time.slice(0, 2)}:${time.slice(2, 4)}:00`;
+}
+
 const TimeRange = ({ handleInput, from, to }) => {
     const { t } = useTranslation();
-    const fromEl = useRef();
-    const toEl = useRef();
 
-    const formatTime = (time) => {
-        return `${time.slice(0, 2)}:${time.slice(2, 4)}:00`;
-    }
-
-    useEffect(() => {
-        if (from) fromEl.current.value = formatTime(from);
-        if (to) toEl.current.value = formatTime(to);
-    }, [])
 
     return (
         <div className={styles.container}>
@@ -25,17 +19,17 @@ const TimeRange = ({ handleInput, from, to }) => {
 
             <div className={styles.fields}>
                 <input
-                    ref={fromEl}
                     className={styles.time}
                     type="time"
+                    defaultValue={from ? formatTime(from) : null}
                     onChange={e => handleInput({ type: 'from', value: e.target.value.replace(':', '') })} />
                 
                 <span>tot</span>
 
                 <input
-                    ref={toEl}
                     className={styles.time}
                     type="time"
+                    defaultValue={to ? formatTime(to) : null}
                     onChange={e => handleInput({ type: 'to', value: e.target.value.replace(':', '') })} />
             </div>
         </div>
