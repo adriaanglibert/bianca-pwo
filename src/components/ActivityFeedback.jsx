7a583daca205de8f8e66f6acc44d0632c577657a
Adriaan Glibert <adriaan.glibert@arteveldehs.be>
2022-02-04 08:55:11 +0100

    ✨ Add labels to activities and warning to day

--- src/components/ActivityFeedback.jsx
+++ src/components/ActivityFeedback.jsx
@@ -1,24 +1,49 @@
-import { DAY_INTENSITY_PERCENTAGE, MAX_DAY_KCAL } from 'constants/values';
+import { DAY_INTENSITY_PERCENTAGE, MAX_DAY_KCAL } from "constants/values";
 
-import React from 'react';
+import { FiAlertTriangle } from "react-icons/fi";
+import IconModal from "components/IconModal";
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 const showMessage = (value) => {
-    const coefficient = 100 / MAX_DAY_KCAL;
-    const percentage = value * coefficient;
-    let message;
-    for (const key in DAY_INTENSITY_PERCENTAGE) {
-        if (percentage < DAY_INTENSITY_PERCENTAGE[key]) {
-            message = key;
-            break;
-        }
+  const coefficient = 100 / MAX_DAY_KCAL;
+  const percentage = value * coefficient;
+  let message = 'heavy';
+  for (const key in DAY_INTENSITY_PERCENTAGE) {
+    if (percentage < DAY_INTENSITY_PERCENTAGE[key]) {
+      message = key;
+      break;
     }
-    return message;
-}
+  }
+  return message;
+};
 
-const ActivityFeedback = ({weekTotal = 0}) => {
-  return <div>
-      <span>{showMessage(weekTotal)}</span>
-  </div>;
+const Modal = ({ message, icon }) => {
+  const { t } = useTranslation();
+  console.log(message);
+  return (
+    message && (
+      <IconModal
+        icon={icon}
+        variant={message}
+        title={t(`week.${message}.title`)}
+      >
+        {t(`week.${message}.description`)}
+      </IconModal>
+    )
+  );
+};
+
+const ActivityFeedback = ({ weekTotal = 0, className }) => {
+  const message = showMessage(weekTotal);
+
+  return (
+    <div className={className}>
+      {(message === "strong" || message === "heavy") && (
+        <Modal message={message} icon={<FiAlertTriangle />} />
+      )}
+    </div>
+  );
 };
 
 export default ActivityFeedback;
--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,74 +1,87 @@
-import Button from './Button';
+import Button from "./Button";
 import { FiX } from "react-icons/fi";
-import Heading from './Heading';
-import Modal from 'react-modal';
-import React from 'react'
-import { defaultStyles } from 'react-modal';
-import styles from 'components/Dialog.module.scss';
-import { useTranslation } from 'react-i18next';
+import Heading from "./Heading";
+import Modal from "react-modal";
+import React from "react";
+import { defaultStyles } from "react-modal";
+import general from "styling/general.module.scss";
+import styles from "components/Dialog.module.scss";
+import { useTranslation } from "react-i18next";
 
 const customStyles = {
-    ...defaultStyles,
-    content: {
-        border: 'none',
-        borderRadius: '0.4rem',
-        boxShadow: '0.25rem 0.25rem 0.5rem rgba(16,16,16, 0.10)',
-        padding: '0',
-        minWidth: '320px',
-        maxWidth: '320px',
-        maxHeight: '90%',
-        position: 'relative',
-        inset: '0',
-        overflow: 'visible'
-    },
-    overlay: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(16, 16, 16, 0.75)',
-        padding: '1rem'
-    },
+  ...defaultStyles,
+  content: {
+    border: "none",
+    borderRadius: "0.4rem",
+    boxShadow: "0.25rem 0.25rem 0.5rem rgba(16,16,16, 0.10)",
+    padding: "0",
+    minWidth: "320px",
+    maxWidth: "320px",
+    maxHeight: "90%",
+    position: "relative",
+    inset: "0",
+    overflow: "visible",
+  },
+  overlay: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(16, 16, 16, 0.75)",
+    padding: "1rem",
+  },
 };
 
-const Dialog = ({ title, variant, children, open = false, setOpen, icon, actions = false, onClose }) => {
-    const { t } = useTranslation();
+const Dialog = ({
+  title,
+  variant = 'primary',
+  children,
+  open = false,
+  setOpen,
+  icon,
+  actions = false,
+  onClose,
+}) => {
+  const { t } = useTranslation();
 
-    return (
-        <Modal
-            isOpen={open}
-            onRequestClose={() => setOpen(false)}
-            contentLabel={title}
-            style={customStyles}
-            shouldCloseOnOverlayClick={true}
-            onAfterClose={onClose}
-        >
-            {
-                title &&
-                <div className={styles.header}>
-                    {icon && <span className={styles.icon}>{icon}</span>}
-                    <Heading variant={variant} level={2}>
-                        {title}
-                    </Heading>
-                </div>
-            }
+  return (
+    <Modal
+      isOpen={open}
+      onRequestClose={() => setOpen(false)}
+      contentLabel={title}
+      style={customStyles}
+      shouldCloseOnOverlayClick={true}
+      onAfterClose={onClose}
+    >
+      {title && (
+        <div className={styles.header}>
+          {icon && (
+            <span className={`${styles.icon} ${general["text-" + variant]}`}>
+              {icon}
+            </span>
+          )}
+          <Heading variant={variant} level={2}>
+            {title}
+          </Heading>
+        </div>
+      )}
 
-            <article className={styles.content}>
-                {children}
-            </article>
+      <article className={styles.content}>{children}</article>
 
-            <div className={styles.actions}>
-                {actions ?
-                    actions :
-                    <Button variant="light" onClick={() => setOpen(false)}>
-                        {t('close')}
-                    </Button>}
-            </div>
+      <div className={styles.actions}>
+        {actions ? (
+          actions
+        ) : (
+          <Button variant="light" onClick={() => setOpen(false)}>
+            {t("close")}
+          </Button>
+        )}
+      </div>
 
-            <button onClick={() => setOpen(false)} className={styles.close}>
-                <FiX />
-            </button>
-        </Modal>
-    )
-}
+      <button onClick={() => setOpen(false)} className={styles.close}>
+        <FiX />
+      </button>
+    </Modal>
+  );
+};
 
-export default Dialog
+export default Dialog;
